7329642cd545e38eb00af38bf46a74ab213cef2c
hoojianliang <hoojianliang@users.noreply.github.com>
2022-02-17 16:42:49 +0800

    implement locale

---
+++ src/components/TodoList.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>{{ $t("message") }}</h1>
+  <LocaleSwitcher />
+</template>
+
+<script>
+import LocaleSwitcher from "./LocaleSwitcher.vue";
+
+export default {
+  name: "TodoList",
+  components: { LocaleSwitcher },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
+import i18n from "./i18n";
 
-createApp(App).use(store).mount("#app");
+createApp(App).use(i18n).use(store).mount("#app");
